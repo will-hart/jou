0a1f434ad03a0a1cfb56e71581f402c89b5d9625
Will Hart <hart.wl@gmail.com>
2020-04-13 19:54:41 +1000

    chore: incorrectly committed lib files

--- packages/demo/lib/src/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { Game } from 'boardgame.io';
-export interface IGameState {
-    cells: (string | null)[];
-}
-declare const DemoGame: Game<IGameState>;
-export default DemoGame;
--- packages/server/lib/index.js
+++
@@ -1,6 +0,0 @@
-// const Server = require('boardgame.io/server').Server
-import { Server } from 'boardgame.io/server';
-import DemoGame from '@jou/demo/src';
-const server = Server({ games: [DemoGame] });
-server.run(8000);
-export default server;
